a70cf13ecb29892e394f1e4b08124e189a65e417
cndrxxxx <48991349+cndrxxxx@users.noreply.github.com>
2019-04-09 22:03:16 +1000

    Create Tut8EX2.java

---
+++ Tut8EX2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Tut8EX2{
+	
+	public static void main(String[] args){
+		//System.out.println("123");
+		ArrayList<Animal> animalArrayList = new ArrayList<>();
+		
+		for (Animal animal: animalArrayList)
+		{
+	         animal.makeSound();
+        }
+		
+	}
+	
+}
+
+
